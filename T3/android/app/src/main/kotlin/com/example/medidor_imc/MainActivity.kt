35370f22e2557787c06c1d75a72888f5992251e2
AbrahamHub <apple_abraham@hotmail.com>
2024-09-03 21:10:14 -0600

    App ready

--- T3/android/app/src/main/kotlin/com/example/medidor_imc/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.medidor_imc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- T3/lib/main.dart
+++ T3/lib/main.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:medidor_imc/ui/calculator/ui_calculator.dart';
+import 'package:imc_calculator/imccalculator.dart';
+import 'package:imc_calculator/imcprovider.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
 
-void main() {
-  runApp(const MyApp());
+
+
+void main() => runApp(AppState());
+
+class AppState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    WidgetsFlutterBinding.ensureInitialized();
+   
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => IMCProvider(),
+        ),
+ 
+      ],
+      child: MyApp(),
+    );
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: CalculatorUI(),
-    );
+    return Sizer(builder: (context, orientation, deviceType) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Calculadora del IMC',
+        home: IMCCalculator(),
+      );
+    });
   }
 }
